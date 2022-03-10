63b09b2358f05740444d3b8cbfec858a89870532
taekow <taeko.owaki@outlook.com>
2022-03-10 12:01:23 -0800

    Added Ninja and Sensei bonuses

--- Week1/Extra Practice/AlfredBot/AlfredQuotes.java
+++ Week1/Extra Practice/AlfredBot/AlfredQuotes.java
@@ -1,3 +1,4 @@
+import java.time.LocalTime;
 import java.util.Date;
 public class AlfredQuotes {
     
@@ -13,16 +14,52 @@ public class AlfredQuotes {
     
     public String dateAnnouncement() {
         // YOUR CODE HERE
-        return "place holder for date announcement return string";
+        Date date = new Date();
+        return String.format("t is currently %s", date);
     }
     
     public String respondBeforeAlexis(String conversation) {
         // YOUR CODE HERE
-        return "for snarky response return string";
+
+        if (conversation.indexOf("Alexis") > -1) {
+            return "Right away, sir. She certainly isn't sophisticated enough for that.";
+        }
+
+        if (conversation.indexOf("Alfred") > -1 ) {
+            return "At your service. As you wish, naturally.";
+        }
+
+        return "Right. And with that I shall retire.";
     }
     
 	// NINJA BONUS
 	// See the specs to overload the guessGreeting method
+    public String guestGreeting(String datePeriod, String name) {
+        return String.format("%s %s. Lovely to see you", datePeriod, name);
+
+    }
     // SENSEI BONUS
     // Write your own AlfredQuote method using any of the String methods you have learned!
+    public String guestGreetingWithDatePeriod(String name) {
+        //DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime datetime = LocalTime.now();
+
+        String datePeriod;
+
+        // To show good morning
+        if (datetime.getHour() > 4 && datetime.getHour() < 12) {
+             datePeriod = "Good morning";
+        }
+        // To show good afternoon
+        else if (datetime.getHour() > 12 && datetime.getHour() < 17) {
+            datePeriod = "Good afternoon";
+        }
+
+        // To show good evening
+        else {
+            datePeriod = "Good evening";
+        }
+
+        return String.format("%s %s. Lovely to see you", datePeriod, name);
+    }
 }

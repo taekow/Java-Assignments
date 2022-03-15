d758efe6b639b97b9f1dac59a12647475aea3e5b
taekow <taeko.owaki@outlook.com>
2022-03-14 22:50:30 -0700

    Add methos

--- Week2/Extra Practice/BaristasChallenge/Order.java
+++ Week2/Extra Practice/BaristasChallenge/Order.java
@@ -1,3 +1,4 @@
+import java.lang.annotation.Retention;
 import java.util.ArrayList;
     
 // Here we're creating a new data type called Order
@@ -10,17 +11,52 @@ public class Order {
     
     // CONSTRUCTOR
     // No arguments, sets name to "Guest", initializes items as an empty list.
-    
+    public order() {
+        this.name = "guest";
+        this.ready = false;
+    }
+
     // OVERLOADED CONSTRUCTOR
     // Takes a name as an argument, sets name to this custom name.
     // Initializes items as an empty list.
+    public order(String name) {
+        this.name = name;
+        this.ready = false;
+    }
     
     // ORDER METHODS
+    // getter
+    public String getName(String name) {
+        return this.name;
+    }
+
+    public boolean getReady() {
+        return this.ready;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    // setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReady(boolean ready) {
+        this.ready = ready;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+    // Add to array list
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
     
-    	// Most of your code will go here, 
-    	// so implement the getters and setters first!
-    
-    // GETTERS & SETTERS
     
 }
 

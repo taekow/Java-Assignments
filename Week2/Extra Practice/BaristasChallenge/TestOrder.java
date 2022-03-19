ff98f5241e0be63ce97066ea7952a7beda5579fc
taekow <taeko.owaki@outlook.com>
2022-03-18 19:01:38 -0700

    Add methods

--- Week2/Extra Practice/BaristasChallenge/TestOrder.java
+++ Week2/Extra Practice/BaristasChallenge/TestOrder.java
@@ -3,10 +3,10 @@ public class TestOrder {
     public static void main(String[] args) {
         
         // Menu items
-        Item item1 = new Item("cappuccino");
-        Item item2 = new Item("Drip Coffee");
-        Item item3 = new Item("Latte");
-        Item item4 = new Item("Mocha Latte");
+        Item item1 = new Item("cappuccino", 3.00);
+        Item item2 = new Item("Drip Coffee",2.20);
+        Item item3 = new Item("Latte",3.50);
+        Item item4 = new Item("Mocha Latte", 3.70);
 
         //Create 2 orders for unspecified guests (without specifying a name);
         Order order1 = new Order();
@@ -19,17 +19,25 @@ public class TestOrder {
 
         // Add at least 2 items to each of the orders using the addItem method you wrote, 
         // for example, to add item1 to order3 you would need to call the addItem method from the order3 instance like so: order3.addItem(item1);
-        Order3.addItem(item2);
-        Order2.addItem(item3);
+        order3.addItem(item2);
+        order2.addItem(item3);
 
         // Implement the getStatusMessage method within the Order class
         System.out.println(order3.getStatusMessage());
 
+        // Test your agetStatusMessage functionality by setting some orders to ready and printing the messages for each order. 
+        // For example: order2.setReady(true); System.out.println(order2.getStatusMessage());
+        order3.setReady(true); 
+        System.out.println(order3.getStatusMessage());
+
         // The total by printing the return value like so: System.out.println(order1.getOrderTotal());
+        System.out.println(order2.getOrderTotal());
 
         // Implement the display method within the Order class
-
-        // call the display method on all of your orders, like so: order3.display();
+        // call the display method on all of your orders, like so: order3.display()
+        // order3.addItem(item1);
+        // order3.addItem(item4);
+        // order3.addItem(item2);
+        // order3.display();
     }
-    
 }

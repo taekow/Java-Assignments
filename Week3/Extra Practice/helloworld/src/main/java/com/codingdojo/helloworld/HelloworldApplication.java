887aa05e9790f01b5ef11d265fe87f7fcdeee2bb
taekow <taeko.owaki@outlook.com>
2022-03-22 16:04:05 -0700

    Add annotations

--- Week3/Extra Practice/helloworld/src/main/java/com/codingdojo/helloworld/HelloworldApplication.java
+++ Week3/Extra Practice/helloworld/src/main/java/com/codingdojo/helloworld/HelloworldApplication.java
@@ -2,12 +2,27 @@ package com.codingdojo.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
+@RestController
+@RequestMapping("/hello")
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+	
+	@RequestMapping("/world")
+	public String world() {
+		return "Class level annotations are cool too!";
 	}
 
 }

4555116a33531660e337b2ef87b28f9c7d2a57a3
taekow <taeko.owaki@outlook.com>
2022-03-22 16:21:51 -0700

    Added daikichi routes assignment

---
+++ Week3/Extra Practice/daikichiroutes/src/main/java/com/codingdojo/daikichiroutes/DaikichiroutesApplication.java
@@ -0,0 +1,33 @@
+package com.codingdojo.daikichiroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiroutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiroutesApplication.class, args);
+	}
+	
+	@RequestMapping("")
+	public String welcome() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("/tomorrow") 
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+
+}
